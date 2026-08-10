a4177f955639d348f8accbca044410d232b2d715
aryansahu06 <aryansahu0606@gmail.com>
2026-08-11 00:28:51 +0530

    remove sorted duplicate array

--- solution.cpp
+++ solution.cpp
@@ -1,34 +1,29 @@
-// check the array is sorted or not
-
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
+int main()
+{
 
-
-
-
-int main() {
-    
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i = 0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    for(int i = 1 ; i<n ; i ++){
-        if(arr[i]>=arr[i-1]){
-           
+    int i = 0;
+    for (int j = 1; j < n; j++)
+    {
+        if (arr[j] != arr[j - 1])
+        {
+            arr[i + 1] = arr[j];
+            i++;
         }
-       else{
-        cout<<"false";
-        break;
     }
-      cout<<"true";
-      break;
-    
-}
-   
+    for (int j = 0; j <= i; j++)
+    {
+        cout << arr[j] << " ";
+    }
+
     return 0;
 }
-
