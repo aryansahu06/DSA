28f8274f867ccebae22d5628fc401a7f05ec0f84
aryansahu06 <aryansahu0606@gmail.com>
2026-08-15 00:32:51 +0530

    linear search in arrays

--- solution.cpp
+++ solution.cpp
@@ -1,19 +1,11 @@
-#include <stdio.h>
-#include <conio.h>
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-// MOVES ZERO TO THE END
+// LINEAR SEARCH 
 
 int main()
 {
     int n;
     cin >> n;
     int arr[n];
-    vector<int> temp;
-    int nz;
+    
     for (int i = 0; i < n; i++)
     {
 
@@ -21,24 +13,11 @@ int main()
     }
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] != 0)
+        if (arr[i] == n)
         {
-            temp.push_back(arr[i]);
+            cout<<"found at index : "<<i ;
         }
     }
-    nz = temp.size();
-    for (int i = 0; i < nz; i++)
-    {
-        arr[i] = temp[i];
-    }
-    for (int i = nz; i < n; i++)
-    {
-        arr[i] = 0;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+   
     return 0;
 }
