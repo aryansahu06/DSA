194f45be5ff13b19461c315da630e2125e1992f7
aryansahu06 <aryansahu0606@gmail.com>
2026-08-24 18:31:29 +0530

    two sum problem

--- solution.cpp
+++ solution.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
+int longestarray(int n, vector<int> &book, int target)
+{
+    int left = 0;
+    int right = n - 1;
+    sort(book.begin(), book.end());
+
+    while (left < right)
+    {
+        int sum = book[left] + book[right];
+        if (sum == target)
+        {
+            cout << "YES";
+            return 1;
+        }
+        else if (sum < target)
+        {
+            left++;
+        }
+
+        else
+        {
+            right--;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
 int main()
 {
     int n = 5;
-    int arr[n] = {1, 2, 3, 4, 5};
-    int xorr = 0;
-    for(int i = 0 ; i<n; i++){
-        xorr = xorr ^ arr[i];
-        
-    }
-    cout<<xorr;
-    
+    vector<int> book = {2, 4, 5, 6, 9};
+    int target = 10;
+
+    longestarray(n, book, target);
+
     return 0;
 }
