889f6e43d1aa64e4c00026a5d6c0701eb6c40b6f
aryansahu06 <aryansahu0606@gmail.com>
2026-09-17 00:56:40 +0530

    leader's in array

--- solution.cpp
+++ solution.cpp
@@ -1,45 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
-
-using namespace std;
-
-vector<int> Rearrrangearray(vector<int> &arr)
-{
-    int n = arr.size();
-    int positivearray = 1;
-    int negativearray = 0;
-    vector<int> ans(n, 0);
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] < 0)
-        {
-            ans[negativearray] = arr[i];
-            negativearray += 2;
-        }
-        else
-        {
-            ans[positivearray] = arr[i];
-            positivearray += 2;
-        }
-    }
-    return ans;
-}
-
 int main()
 {
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    vector<int> ans = Rearrrangearray(arr);
-    for (int i = 0; i < n; i++)
-    {
-        cout << ans[i] << " ";
+    int arr[] = {18,22,8,9,15,4,3};
+    int n = 7;
+    int max = arr[n-1];
+    cout<< max << " ";
+    for(int i = n-2; i>=0 ; i--){
+        if(arr[i]>max){
+            max = arr[i];
+            cout<<max<< " ";
+        }
     }
 
     return 0;
