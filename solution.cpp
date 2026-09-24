73328d223feb51623aae3e88b02b58ca29255ade
aryansahu06 <aryansahu0606@gmail.com>
2026-09-25 00:35:46 +0530

    Binary search

--- solution.cpp
+++ solution.cpp
@@ -1,51 +1,36 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void reverse(vector<int> &row)
-{
-    int start = 0;
-    int end = row.size() - 1;
 
-    while (start < end)
-    {
-        swap(row[start], row[end]);
-        start++;
-        end--;
-    }
-}
-void Rotatematrix(vector<vector<int>> &matrix)
+int BinarySearch(vector<int> &nums , int target)
 {
-    int n = matrix.size();
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            swap(matrix[i][j], matrix[j][i]);
-        }
-    }
+    int n = nums.size();
 
-    for (int i = 0; i < n; i++)
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right)
     {
-        reverse(matrix[i]);
+        int mid = (left + right) / 2;
+        if (nums[mid] == target)
+            return mid;
+
+        else if (nums[mid] < target)
+            left = mid + 1;
+
+        else
+            right = mid - 1;
     }
+    return -1;
 }
 
 int main()
 {
+    
+    vector <int> arr = {1,3,5,6,9,11,13};
+    int target = 9;
+    int result = BinarySearch(arr,target);
+    cout<<"Element found at index: "<< result <<endl;
+    return 0;
 
-    vector<vector<int>> matrix = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}};
-
-    Rotatematrix(matrix);
-
-    for (int i = 0; i < matrix.size(); i++)
-    {
-        for (int j = 0; j < matrix[i].size(); j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
